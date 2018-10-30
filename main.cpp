803609814a6e7469fcba70c7643cba187a03b4e2
Nigel Evans <nigel@amarula-electronics.com>
2018-10-30 22:18:51 +0000

    Add basic Splash Screen. Need custom Image.
    Signed-off-by: Nigel Evans <nigel@amarula-electronics.com>

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <wx/wx.h>
 #include <wx/log.h> 
+#include "wx/image.h"
+#include "wx/splash.h"
 
 class Simple : public wxFrame
 {
@@ -17,6 +19,19 @@ public:
 	bool OnInit()
 	{
 		Simple *simple = new Simple(wxT("Elipsus 1.0.0"));
+
+		wxImage::AddHandler(new wxPNGHandler);
+
+		wxBitmap bitmap;
+		bitmap.LoadFile(wxT("splash.png"), wxBITMAP_TYPE_PNG);
+
+		new wxSplashScreen(bitmap,
+                wxSPLASH_CENTRE_ON_SCREEN|wxSPLASH_TIMEOUT,
+                6000, simple, wxID_ANY, wxDefaultPosition, wxDefaultSize,
+                wxSIMPLE_BORDER|wxSTAY_ON_TOP);
+
+		wxYield();
+
 		simple->Show(true);
 		wxLogDebug(wxT("App Init Success!"));
 		return true;
